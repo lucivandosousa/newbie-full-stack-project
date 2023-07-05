d74c09344f2f63e8a6343cb7620368f22c0154d6
Lucivando Sousa <lucivandosousa@gmail.com>
2023-07-05 14:28:28 -0300

    adicionar endpoint para cadastro e selecao por id

--- front-end/app.js
+++ front-end/app.js
@@ -15,9 +15,29 @@ app.set('view engine', 'handlebars')
 app.set('views', './views')
 
 app.get('/', (req, res) => {
-  fetch('http://localhost:3000/clientes')
+  fetch('http://localhost:3000/clientes', {method: 'GET'})
   .then(response => response.json())
   .then(clientes => res.render('home', {clientes}))
+})
+
+app.get('/clientes/:id', (req, res) => {
+  const id = req.params.id
+
+  fetch(`http://localhost:3000/clientes/${id}`, {method: 'GET'})
+  .then(response => response.json())
+  .then(cliente => res.render('selecionar', {cliente}))
+})
+
+app.post('/cadastrar', (req, res) => {
+  const {nome, idade} = req.body
+  const cliente = {"nome": nome, "idade": idade}
+
+  fetch('http://localhost:3000/clientes', {
+    method: 'POST',
+    headers: {'Content-Type': 'application/json'},
+    body: JSON.stringify(cliente) 
+  })
+  .then(res.redirect('/'))
 })
 
 app.listen(port)
